0da5171cb601ef830b6769471c219efb609c11dd
farisaziz12 <farisaziz12@gmail.com>
2020-06-09 21:56:35 +0200

    bugsnag and firebase integrated but not fully setup

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import Bugsnag from "@bugsnag/js";
+import BugsnagPluginReact from "@bugsnag/plugin-react";
+
+Bugsnag.start({
+  apiKey: "268294667dccd97f36132509e9bc2c5d",
+  plugins: [new BugsnagPluginReact()],
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
